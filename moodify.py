d5d7e1143f753a13ae562814111de995a1c1dade
jaydeep patel <132196204+jaydeeppatel17@users.noreply.github.com>
2023-05-03 19:49:09 +0530

    Update moodify.py

--- moodify.py
+++ moodify.py
@@ -1,29 +1,43 @@
+from flask import Flask, request, jsonify
 import cv2
 import numpy as np
 from keras.models import load_model
 
-label_dictionary = {0:"Angry", 1:"Disgust", 2:"Fear", 3:"Happy", 4:"Neutral", 5:"Sad", 6:"Surprise"}
+app = Flask(__name__)
 
 # Load the model
 moodDetector = load_model("moodifyEngine.h5")
 
-# Load the image
-img = cv2.imread('sadladki.jpg')
+# Define a dictionary to map class indices to emotion labels
+label_dictionary = {0: "Angry", 1: "Disgust", 2: "Fear", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprise"}
 
+# Define a helper function to preprocess the image
+def preprocess_image(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, (48, 48))
+    normalized = resized / 255.0
+    return normalized.reshape((1,48,48,1))
 
+# Define the prediction route
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the image file from the request
+    file = request.files['image']
+    
+    # Read the image as a numpy array
+    image = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_COLOR)
+    
+    # Preprocess the image
+    input_data = preprocess_image(image)
+    
+    # Make a prediction using the model
+    prediction = np.argmax(moodDetector.predict(input_data), axis=-1)
+    
+    # Get the corresponding emotion label
+    emotion = label_dictionary.get(prediction[0])
+    
+    # Return the prediction as a JSON response
+    return jsonify({'emotion': emotion})
 
-# Preprocess image
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-resized = cv2.resize(gray, (48, 48))
-normalized = resized / 255.0
-n = reshape_and_rotate(normalized)
-input_data = n.reshape((1,48,48))
-
-# Make prediction
-prediction = np.argmax(moodDetector.predict(input_data), axis=-1)
-
-# Print result
-print("The predicted emotion is:", label_dictionary.get(prediction[0]))
-
-# Show image
-
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
